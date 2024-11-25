c1753cc4baadeed8f93aea49f75ab7e60f4b3d25
User <sachinamz857@gmail.com>
2024-11-26 00:58:42 +0530

    ui

--- app/src/main/java/com/example/akhleshkumar/homedootpartner/activities/MainActivity.kt
+++ app/src/main/java/com/example/akhleshkumar/homedootpartner/activities/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.akhleshkumar.homedootpartner.activities
 import android.app.ProgressDialog
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.telecom.Call
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.akhleshkumar.homedoot.api.RetrofitClient
+import com.example.akhleshkumar.homedootpartner.R
 import com.example.akhleshkumar.homedootpartner.databinding.ActivityMainBinding
 import com.example.akhleshkumar.homedootpartner.models.VendorDashboardResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+
     lateinit var progressDialog: ProgressDialog
     lateinit var sharedPreferences: SharedPreferences
     lateinit var editorSP : SharedPreferences.Editor
@@ -21,31 +25,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Close Drawer
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+            // Show Toast based on clicked item
+            when (menuItem.itemId) {
+                R.id.menu_calendar -> showToast("Calendar Clicked")
+                R.id.menu_job_history -> showToast("Job History Clicked")
+                R.id.menu_profile -> showToast("My Profile Clicked")
+                R.id.menu_business_detail -> showToast("Update Business Detail Clicked")
+                R.id.menu_bank_detail -> showToast("Update Bank Detail Clicked")
+                R.id.menu_rating -> showToast("Rating Clicked")
+                R.id.menu_commission -> showToast("Commission Clicked")
+                else -> showToast("Unknown Item Clicked")
+            }
+            true
+        }
+
+
         sharedPreferences = getSharedPreferences("HomeDoot", MODE_PRIVATE)
         editorSP = sharedPreferences.edit()
         progressDialog = ProgressDialog(this).apply {
             setMessage("Loading...")
             setCancelable(false)
         }
-        getVendorDashboard(sharedPreferences.getString("userId","").toString())
+       // getVendorDashboard(sharedPreferences.getString("userId","").toString())
     }
 
-    private fun getVendorDashboard(userId:String) {
-        RetrofitClient.instance.getVendorDashboard(userId).enqueue(object : Callback<VendorDashboardResponse> {
-            override fun onResponse(
-                call: Call<VendorDashboardResponse>,
-                response: Response<VendorDashboardResponse>
-            ) {
-                if (response.isSuccessful){
-
-                    Toast.makeText(this@MainActivity, response.body()?.message, Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onFailure(call: Call<VendorDashboardResponse>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "something went wrong", Toast.LENGTH_SHORT).show()
-            }
-
-        })
+//    private fun getVendorDashboard(userId:String) {
+//        RetrofitClient.instance.getVendorDashboard(userId).enqueue(object : Callback<VendorDashboardResponse> {
+//            override fun onResponse(
+//                call: Call<VendorDashboardResponse>,
+//                response: Response<VendorDashboardResponse>
+//            ) {
+//                if (response.isSuccessful){
+//
+//                    Toast.makeText(this@MainActivity, response.body()?.message, Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<VendorDashboardResponse>, t: Throwable) {
+//                Toast.makeText(this@MainActivity, "something went wrong", Toast.LENGTH_SHORT).show()
+//            }
+//
+//        })
+//    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
